400b471fc3fba66b60d15b8e10a907530e32a415
Max Rose <maxrose93@hotmail.com>
2016-05-04 10:49:31 -0700

    final commit

--- survey.py
+++ survey.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-# our index route will handle rendering our form
+
 @app.route('/')
 def index():
   return render_template("index.html")
-# this route will handle our form submission
-# notice how we defined which HTTP methods are allowed by this route
-@app.route('/users', methods=['POST'])
-def create_user():
-   print "Got Post Info"
-   # we'll talk about the following two lines after we learn a little more
-   # about forms
-   name = request.form['name']
-   email = request.form['email']
-   # redirects back to the '/' route
-   return redirect('/')
-app.run(debug=True) # run our server
+
+
+@app.route('/process', methods=['POST'])
+def submit_info():
+	name = request.form['name']
+	location = request.form['location']
+	lang = request.form['lang']
+	comment = request.form['comment']
+	return render_template("results.html", nom=name, loc=location, language=lang, com=comment)
+
+app.run(debug=True)
